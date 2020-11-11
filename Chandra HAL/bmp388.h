931e84eb15f152bf83cbb9b646b0b440193c5104
Martin McKee <martinjaymckee@gmail.com>
2020-11-10 19:45:45 -0700

    Added a basic (functional) implementation of the driver for the BMP388 and skeletal driver for the BMX160 (which compiles, but is non-functional).  Soon this needs to remove the old non-CRTP based inertial code and rename the _crtp headers without the suffix.

---
+++ Chandra HAL/bmp388.h
@@ -0,0 +1,470 @@
+#ifndef CHANDRA_BMP388_H
+#define CHANDRA_BMP388_H
+
+//
+// Standard Includes
+//
+#include <chrono>
+
+//
+// Chandra Includes
+//
+#include "barometric.h"
+#include "chrono.h"
+#include "i2c.h"
+#include "timing.h"
+#include "register_device.h"
+#include "sensor_utils.h"
+#include "spi.h"
+#include "units.h"
+
+namespace chandra
+{
+namespace drivers
+{
+namespace internal
+{
+template<class Value, class BaseUnits, class Comm>
+struct BMP388ImplBase : BaseBarometricProxyImpl<Value, BaseUnits>
+{
+    // Register Access Type
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    using scalar_t = Value;
+
+    BMP388ImplBase(const register_access_t& _regs, const scalar_t& _scale)
+      : regs_(_regs), scale_(_scale) {}
+
+
+    // Register Addresses
+    enum registers_t {
+        CHIP_ID = 0X00,
+		ERR_REG = 0x02,
+		STATUS = 0x03,
+		PRESS_L = 0x04,
+		PRESS_M = 0x05,
+		PRESS_H = 0x06,
+		TEMP_L = 0x07,
+		TEMP_M = 0x08,
+		TEMP_H = 0x09,
+		SENSOR_TIME_L = 0x0C,
+		SENSOR_TIME_ML = 0x0D,
+		SENSOR_TIME_HM = 0x0E,
+		SENSOR_TIME_H = 0x0F,
+		EVENT = 0x10,
+		INT_STATUS = 0x11,
+		FIFO_LENGTH_L = 0x12,
+		FIFO_LENGTH_H = 0x13,
+		FIFO_DATA = 0x14,
+		FIFO_WATERMARK_L = 0x15,
+		FIFO_WATERMARK_H = 0x16,
+		FIFO_CONFIG_1 = 0x17,
+		FIFO_CONFIG_2 = 0x18,
+		INT_CTRL = 0x19,
+		IF_CONFIG = 0x1A,
+		PWR_CTRL = 0x1B,
+		OSR = 0x1C,
+		ODR = 0x1D,
+		IIR_CONFIG = 0x1F,
+
+		NVM_CRC = 0x30,
+		NVM_T1_L = 0x31,
+		NVM_T1_H = 0x32,
+		NVM_T2_L = 0x33,
+		NVM_T2_H = 0x34,
+		NVM_T3 = 0x35,
+		NVM_P1_L = 0x36,
+		NVM_P1_H = 0x37,
+		NVM_P2_L = 0x38,
+		NVM_P2_H = 0x39,
+		NVM_P3 = 0x3A,
+		NVM_P4 = 0x3B,
+		NVM_P5_L = 0x3C,
+		NVM_P5_H = 0x3D,
+		NVM_P6_L = 0x3E,
+		NVM_P6_H = 0x3F,
+		NVM_P7 = 0x40,
+		NVM_P8 = 0x41,
+		NVM_P9_L = 0x42,
+		NVM_P9_H = 0x43,
+		NVM_P10 = 0x44,
+		NVM_P11 = 0x45,
+
+		CMD = 0x7E
+      };
+
+    auto set_odr(const units::Quantity<scalar_t, units::mks::Hz>&) {
+      return units::Quantity<scalar_t, units::mks::Hz>(0);
+    }
+
+    auto get_odr() {
+      return units::Quantity<scalar_t, units::mks::Hz>(0);
+    }
+
+    auto set_osr(const uint16_t&) {
+      return 0;
+    }
+
+    auto get_osr() {
+      return 0;
+    }
+
+    auto set_lpf(const units::Quantity<scalar_t, units::mks::Hz>&) {
+      return units::Quantity<scalar_t, units::mks::Hz>(0);
+    }
+
+    auto get_lpf() {
+      return units::Quantity<scalar_t, units::mks::Hz>(0);
+    }
+
+
+    constexpr scalar_t scale() const { return scale_; }
+
+  protected:
+    register_access_t regs_;
+    scalar_t scale_;
+};
+
+template<class Value, class BaseUnits, class Comm>
+class BMP388PressureImpl : public BMP388ImplBase<Value, BaseUnits, Comm>
+{
+  public:
+    using scalar_t = Value;
+    using base_t = BMP388ImplBase<Value, BaseUnits, Comm>;
+    using registers_t = typename base_t::registers_t;
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    BMP388PressureImpl(const register_access_t& _regs)
+      : base_t(_regs, 5.98e-4) {}
+};
+
+template<class Value, class BaseUnits, class Comm>
+class BMP388TemperatureImpl : public BMP388ImplBase<Value, BaseUnits, Comm>
+{
+  public:
+    using scalar_t = Value;
+    using base_t = BMP388ImplBase<Value, BaseUnits, Comm>;
+    using registers_t = typename base_t::registers_t;
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    BMP388TemperatureImpl(const register_access_t& _regs)
+      : base_t(_regs, 5.98e-4) {}
+};
+
+template<class Value>
+class BMP388CompensationValues
+{
+	public:
+		BMP388CompensationValues() : valid_(false) {}
+
+		bool update(const uint8_t* _buffer) {
+			// TODO: OPTIMIZE CODE (REMOVE DIVISIONS AT LEAST) AND REMOVE DEPENDENCE ON FLOATING-POINT VALUES
+
+			// Calculate CRC -- If Valid, continue, else return false
+			uint8_t target_crc = _buffer[0];
+			CRC crc;
+			for(uint8_t idx = 1; idx < 22; ++idx) {
+				crc.update(_buffer[idx]);
+			}
+			valid_ = crc.match(target_crc);
+
+
+			// Calculation Compensation Values
+			uint16_t data;
+
+			//	T1
+			data = _buffer[2];
+			data = (data<<8) | _buffer[1];
+			T_[0] = Value(256.0) * static_cast<Value>(data);
+
+			//	T2
+			data = _buffer[4];
+			data = (data<<8) | _buffer[3];
+			T_[1] = static_cast<Value>(data) / Value(1073741824.0);
+
+			//	T3
+			T_[2] = static_cast<Value>(static_cast<int8_t>(_buffer[5])) / Value(2.814749767e14);
+
+			//	P1
+			data = _buffer[7];
+			data = (data<<8) | _buffer[6];
+			P_[0] = (static_cast<Value>(static_cast<int16_t>(data)) - Value(16384.0)) / Value(1048576.0);
+
+			//	P2
+			data = _buffer[9];
+			data = (data<<8) | _buffer[8];
+			P_[1] = (static_cast<Value>(static_cast<int16_t>(data)) - Value(16384.0)) / Value(536870912.0);
+
+			//	P3
+			P_[2] = static_cast<Value>(static_cast<int8_t>(_buffer[10])) / Value(4294967296.0);
+
+			//	P4
+			P_[3] = static_cast<Value>(static_cast<int8_t>(_buffer[11])) / Value(137438953472.0);
+
+			//	P5
+			data = _buffer[13];
+			data = (data<<8) | _buffer[12];
+			P_[4] = Value(8.0) * static_cast<Value>(data);
+
+			//	P6
+			data = _buffer[15];
+			data = (data<<8) | _buffer[14];
+			P_[5] = static_cast<Value>(data) / Value(64.0);
+
+			//	P7
+			P_[6] = static_cast<Value>(static_cast<int8_t>(_buffer[16])) / Value(256.0);
+
+			//	P8
+			P_[7] = static_cast<Value>(static_cast<int8_t>(_buffer[17])) / Value(32768.0);
+
+			//	P9
+			data = _buffer[19];
+			data = (data<<8) | _buffer[18];
+			P_[8] = static_cast<Value>(static_cast<int16_t>(data)) / Value(2.814749767e14);
+
+			//	P10
+			P_[9] = static_cast<Value>(static_cast<int8_t>(_buffer[20])) / Value(2.814749767e14);
+
+			//	P11
+			P_[10] = static_cast<Value>(static_cast<int8_t>(_buffer[21])) / Value(3.68934881474e19);
+
+			return valid_;
+		}
+
+		Value T(const uint8_t& _idx) const { return T_[_idx-1]; }
+		Value P(const uint8_t& _idx) const { return P_[_idx-1]; }
+		bool valid() const { return valid_; }
+
+	protected:
+		class CRC
+		{
+			protected:
+				static const uint8_t poly = 0x1D;
+
+			public:
+				CRC() : crc_(0xFF) {}
+
+				bool reset() {
+					crc_ = 0xFF;
+					return true;
+				}
+
+				uint8_t update(uint8_t _val) {
+					for(uint8_t idx = 0; idx < 8; ++idx) {
+						const bool do_xor = ((crc_ ^ _val) & 0x80) == 0x80;
+						crc_ = (crc_ & 0x7F) << 1;
+						_val = (_val & 0x7F) << 1;
+						if(do_xor) crc_ ^= poly;
+					}
+
+					return crc();
+				}
+
+				uint8_t crc() const { return 0xFF ^ crc_; }
+
+				bool match(const uint8_t _crc) {
+					const uint8_t ref = crc();
+					return (ref == _crc);
+				}
+
+			protected:
+				uint8_t crc_;
+		};
+
+		Value T_[3];
+		Value P_[11];
+		bool valid_;
+};
+
+} /*namespace internal*/
+
+template<
+	class Value,
+	class Comm = chandra::io::SPIMaster,
+	class PressureUnits=chandra::units::mks::hPa,
+	class TemperatureUnits=chandra::units::mks::degC
+>
+class BMP388
+		: public Barometric<
+		  	  BMP388<Value, Comm, PressureUnits, TemperatureUnits>,
+			  Value,
+			  PressureUnits, TemperatureUnits,
+			  internal::BMP388PressureImpl<Value, PressureUnits, Comm>,
+			  internal::BMP388TemperatureImpl<Value, TemperatureUnits, Comm>
+		>
+{
+  protected:
+    friend class Barometric<BMP388<Value, Comm, PressureUnits, TemperatureUnits>, Value, PressureUnits, TemperatureUnits, internal::BMP388PressureImpl<Value, PressureUnits, Comm>, internal::BMP388TemperatureImpl<Value, TemperatureUnits, Comm>>;
+    using base_t = Barometric<BMP388<Value, Comm, PressureUnits, TemperatureUnits>, Value, PressureUnits, TemperatureUnits, internal::BMP388PressureImpl<Value, PressureUnits, Comm>, internal::BMP388TemperatureImpl<Value, TemperatureUnits, Comm>>;
+    using scalar_t = typename base_t::scalar_t;
+    using value_t = typename base_t::value_t;
+    using ref_t = BMP388<Value, Comm, PressureUnits, TemperatureUnits>;
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    using clock_t = chandra::chrono::Timer<>::clock_t;
+    using time_point_t = typename clock_t::time_point;
+    using status_t = SensorUpdateStatus<>;
+
+    using temperature_t = chandra::units::Quantity<scalar_t, TemperatureUnits>;
+    using registers_t = typename internal::BMP388ImplBase<Value, PressureUnits, Comm>::registers_t;
+
+  public:
+    BMP388(register_access_t _regs)
+      : base_t{regs_}, regs_{_regs} {}
+
+    bool init() {
+    	if(!reset()) return false;
+
+		if(!loadCalibrationValues()) return false;
+
+    	//0x19 - INT_CTRL (drdy_en(6), int_level(1))
+    	//0x1B - PWR_CTRL (pressure enable/temperature enable/normal mode)
+    	//0x1C - OSR
+    	//0x1D - ODR (prescaler=1, odr_sel=0x00)
+    	//
+    	regs_.write(registers_t::INT_CTRL, static_cast<uint8_t>((1<<6)|(1<<1))); // Enable DRDY, Active-High
+    	regs_.write(registers_t::OSR, static_cast<uint8_t>(0x0D)); // Pressure x32, Temperature 2x Oversample
+    	regs_.write(registers_t::ODR, static_cast<uint8_t>(4)); // ODR = 12.5Hz
+
+
+    	regs_.write(registers_t::PWR_CTRL, static_cast<uint8_t>(0x33)); // Pressure and Temperature on, Normal Mode
+    	initialized_ = valid();
+    	return initialized_;
+    }
+
+    bool initialized() const { return initialized_;}
+
+    uint8_t id() const {
+    	uint8_t buffer[2];
+    	regs_.bytes(registers_t::CHIP_ID, 2, buffer);
+    	return buffer[1];
+    }
+
+    bool reset() {
+    	constexpr auto timeout_period = std::chrono::milliseconds{15};
+    	const auto t_start = chandra::chrono::timestamp_clock::now();
+    	// TODO: CHECK THAT THE BMP388 IS READY TO RECEIVE A COMMAND
+    	regs_.write(registers_t::CMD, static_cast<uint8_t>(0xB6));
+    	uint8_t buffer[2];
+    	while(true) {
+    		regs_.bytes(registers_t::EVENT, 2, buffer);
+    		if(buffer[1] & 0x01) return true;
+    		if(chandra::chrono::after(timeout_period, t_start)) break;
+    	}
+
+    	return false;
+    }
+
+    bool calibrate(bool _blocking = false) {
+    	calibrated_ = true;
+    	return true;
+    }
+
+    bool calibrated() const { return calibrated_; }
+
+    const auto& CompensationValues() const {return C;}
+
+    status_t update(const bool& _update, const bool& _forced) {
+    	status_t status;
+    	uint8_t buffer[7];
+      // Check if new data is available by reading INT_STATUS
+
+      bool data_available = _update;
+      if(!_forced) {
+    	  regs_.bytes(registers_t::INT_STATUS, 2, buffer);
+    	  data_available = bool(buffer[1] & (1<<3));
+      }
+
+      if(data_available) {
+    	  // Read Pressure and Temperature
+    	  regs_.bytes(registers_t::PRESS_L, 7, buffer);
+
+    	  // Assemble Raw Values
+    	  const uint32_t P_raw = (static_cast<uint32_t>(buffer[3])<<16) | (static_cast<uint32_t>(buffer[2])<<8) | buffer[1];
+    	  const uint32_t T_raw = (static_cast<uint32_t>(buffer[6])<<16) | (static_cast<uint32_t>(buffer[5])<<8) | buffer[4];
+
+    	  // Calculate Corrected Values
+    	  status.calculated = calculatePressureTemperature(P_raw, T_raw);
+    	  status.processed = true;
+      }
+
+      return status;
+    }
+
+    status_t update() {
+    	return update(false, false);
+    }
+
+    status_t update(const bool& _update) {
+    	return update(_update, true);
+    }
+
+    bool valid() const {
+    	return C.valid() and (id() == 0x50);
+    }
+
+    // TODO: THESE SHOULD BE MOVED TO THE BASE CLASS
+    template<class U>
+    temperature_t setSelfHeating(const chandra::units::Quantity<Value, U>& _Tinit) {
+    	const auto Tinit = chandra::units::convert<TemperatureUnits, U>(_Tinit);
+//      Tselfheating_ = temperature_val_ - Tinit_;
+      return temperature_t{Tselfheating_};
+    }
+
+    temperature_t setSelfHeating() {
+//      Tselfheating_ = temperature_val_ - Tinit_;
+      return temperature_t{Tselfheating_};
+    }
+
+    temperature_t Tinit() const { return temperature_t{Tinit_}; }
+    temperature_t Tselfheating() const { return temperature_t{Tselfheating_}; }
+
+  protected:
+    bool loadCalibrationValues(){
+    	static constexpr uint8_t N = 23;
+    	uint8_t buffer[N];
+    	regs_.bytes(registers_t::NVM_CRC, N, buffer);
+    	C.update(&buffer[1]);
+    	return C.valid();
+    }
+
+    bool calculatePressureTemperature(const uint32_t& _P_raw, const uint32_t& _T_raw) {
+    	// Calculate Compensated Temperature
+    	const auto C1 = (_T_raw - C.T(1));
+    	const auto C2 = (C.T(2) * C1);
+    	this->temperature_raw_ = Value{C2 + (C.T(3) * C1 * C1)};
+
+    	// Calculate Compensated Pressure
+    	const auto T_squared = this->temperature_raw_ * this->temperature_raw_;
+    	const auto T_cubed = this->temperature_raw_ * T_squared;
+    	const auto P_squared = _P_raw * _P_raw;
+    	const auto P_cubed = _P_raw * P_squared;
+    	const auto C3 = (C.P(6) * this->temperature_raw_);
+    	const auto C4 = (C.P(7) * T_squared);
+    	const auto C5 = (C.P(8) * T_cubed);
+    	const auto C6 = C.P(5) + C3 + C4 + C5;
+    	const auto C7 = (C.P(2) * this->temperature_raw_);
+    	const auto C8 = (C.P(3) * T_squared);
+    	const auto C9 = (C.P(4) * T_cubed);
+    	const auto C10 = _P_raw * (C.P(1) + C7 + C8 + C9);
+    	const auto C11 = C.P(9) + (C.P(10) * this->temperature_raw_);
+    	const auto C12 = (C11 * P_squared);
+    	const auto C13 = C12 + (C.P(11) * P_cubed);
+    	this->pressure_raw_ = Value{C6 + C10 + C13} / Value{100};
+    	return true;
+    }
+
+  private:
+    bool initialized_ = false;
+    bool calibrated_ = false;
+    chandra::drivers::RegisterDevice<Comm> regs_;
+    internal::BMP388CompensationValues<double> C; // Note: Currently the compensation only works with floating-point values
+    scalar_t Tinit_ = 0;
+    scalar_t Tselfheating_ = 0;
+};
+
+} /* namespace drivers */
+} /* namespace chandra */
+
+
+#endif /*CHANDRA_BMP388_H*/
---
+++ Chandra HAL/bmx160.h
@@ -0,0 +1,313 @@
+#ifndef CHANDRA_BMX160_H
+#define CHANDRA_BMX160_H
+
+
+#include "chrono.h"
+#include "inertial_crtp.h"
+#include "register_device.h"
+#include "sensor_utils.h"
+#include "spi.h"
+#include "thermal.h"
+#include "timing.h"
+
+namespace chandra
+{
+namespace drivers
+{
+namespace internal
+{
+
+template<class Value, class BaseUnits, class Comm>
+class BMX160ImplBase : public chandra::drivers::internal::BaseInertialProxyImpl<Value, BaseUnits>
+{
+  public:
+    using scalar_t = Value;
+
+    // Register Access Type
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    BMX160ImplBase(const register_access_t& _regs, const scalar_t& _scale)
+      : regs_(_regs), scale_(_scale) {}
+
+    // Register Addresses
+    enum registers_t {
+    	CHIP_ID = 0x00,
+		ERR_REG = 0x02,
+		PMU_STATUS = 0x03,
+		MAG_X_L = 0x04,
+		MAG_X_H = 0x05,
+		MAG_Y_L = 0x06,
+		MAG_Y_H = 0x07,
+		MAG_Z_L = 0x08,
+		MAG_Z_H = 0x09,
+		RHALL_L = 0x0A,
+		RHALL_H = 0x0B,
+		GYRO_X_L = 0x0C,
+		GYRO_X_H = 0x0D,
+		GYRO_Y_L = 0x0E,
+		GYRO_Y_H = 0x0F,
+		GYRO_Z_L = 0x10,
+		GYRO_Z_H = 0x11,
+		ACCEL_X_L = 0x12,
+		ACCEL_X_H = 0x13,
+		ACCEL_Y_L = 0x14,
+		ACCEL_Y_H = 0x15,
+		ACCEL_Z_L = 0x16,
+		ACCEL_Z_H = 0x17,
+		SENSOR_TIME_L = 0x18,
+		SENSOR_TIME_M = 0x19,
+		SENSOR_TIME_H = 0x1A,
+		STATUS = 0x1B,
+		INT_STATUS_0 = 0x1C,
+		INT_STATUS_1 = 0x1D,
+		INT_STATUS_2 = 0x1E,
+		INT_STATUS_3 = 0x1F,
+		TEMP_L = 0x20,
+		TEMP_H = 0x21,
+		FIFO_LENGTH_L = 0x22,
+		FIFO_LENGTH_H = 0x23,
+		FIFO_DATA = 0x24,
+		ACCEL_CONFIG = 0x40,
+		ACCEL_RANGE = 0x41,
+		GYRO_CONFIG = 0x42,
+		GYRO_RANGE = 0x43,
+		MAG_CONFIG = 0x44,
+		FIFO_DOWNS = 0x45,
+		FIFO_CONFIG_0 = 0x46,
+		FIFO_CONFIG_1 = 0x47,
+		MAG_IF_0 = 0x4C,
+		MAG_IF_1 = 0x4D,
+		MAG_IF_2 = 0x4E,
+		MAG_IF_3 = 0x4F,
+		INT_EN_0 = 0x50,
+		INT_EN_1 = 0x51,
+		INT_EN_2 = 0x52,
+		INT_OUT_CTRL = 0x53,
+		INT_LATCH = 0x54,
+		INT_MAP_0 = 0x55,
+		INT_MAP_1 = 0x56,
+		INT_MAP_2 = 0x57,
+		INT_DATA_0 = 0x58,
+		INT_DATA_1 = 0x59,
+		INT_LOWHIGH_0 = 0x5A,
+		INT_LOWHIGH_1 = 0x5B,
+		INT_LOWHIGH_2 = 0x5C,
+		INT_LOWHIGH_3 = 0x5D,
+		INT_LOWHIGH_4 = 0x5E,
+		INT_MOTION_0 = 0x5F,
+		INT_MOTION_1 = 0x60,
+		INT_MOTION_2 = 0x61,
+		INT_MOTION_3 = 0x62,
+		INT_TAP_0 = 0x63,
+		INT_TAP_1 = 0x64,
+		INT_ORIENT_0 = 0x65,
+		INT_ORIENT_1 = 0x66,
+		INT_FLAT_0 = 0x67,
+		INT_FLAT_1 = 0x68,
+		FOC_CONFIG = 0x69,
+		CONFIG = 0x6A,
+		IF_CONFIG = 0x6B,
+		PMU_TRIGGER = 0x6C,
+		SELF_TEST = 0x6D,
+		NV_CONFIG = 0x70,
+		ACCEL_OFFSET_X = 0x71,
+		ACCEL_OFFSET_Y = 0x72,
+		ACCEL_OFFSET_Z = 0x73,
+		GYRO_OFFSET_X_L = 0x74,
+		GYRO_OFFSET_Y_L = 0x75,
+		GYRO_OFFSET_Z_L = 0x76,
+		GYRO_OFFSET_ADDITIONAL = 0x77,
+		STEP_CNT_0 = 0x78,
+		STEP_CNT_1 = 0x79,
+
+		CMD = 0x7E
+      };
+
+      constexpr scalar_t scale() const { return scale_; }
+
+    protected:
+//      // TODO: AT LEAST THIS LOG 2 NEEDS TO MOVE TO A UTILITY CLASS....
+//      constexpr uint32_t log2(uint32_t x) const {
+//        //return __builtin_ctz (x);
+//        uint32_t count = 0;
+//        while(x){
+//            x >>= 1;
+//            ++count;
+//        }
+//        return count;
+//      }
+//
+//      constexpr uint8_t calcOdrBits(const Value& _freq) const {
+//        uint8_t bits(0);
+//        if(_freq == 0) {
+//            bits = 0x00;
+//        } else {
+//            const auto div = _freq / 13;
+//            bits = log2(div) + 1;
+//        }
+//        return bits;
+//      }
+//
+//      constexpr uint32_t calcOdrFreq(const uint8_t& bits) const {
+//        return (bits == 0) ? 0 : (13 * (1UL << (bits-1UL)));
+//      }
+
+      register_access_t regs_;
+      scalar_t scale_;
+};
+
+template<class Value, class BaseUnits, class Comm>
+class BMX160AccelImpl : public BMX160ImplBase<Value, BaseUnits, Comm>
+{
+  public:
+    using scalar_t = Value;
+    using base_t = BMX160ImplBase<Value, BaseUnits, Comm>;
+    using registers_t = typename base_t::registers_t;
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    BMX160AccelImpl(const register_access_t& _regs)
+      : base_t(_regs, 5.98e-4) {}
+
+    scalar_t set_fs(scalar_t fs) {
+        return 0;
+    }
+
+    scalar_t get_fs() const {
+    	return 0;
+    }
+
+    scalar_t set_odr(scalar_t _freq) {
+    	return 0;
+    }
+
+    scalar_t get_odr() const {
+    	return 0;
+    }
+
+  protected:
+    scalar_t accel_scale_;
+};
+
+template<class Value, class BaseUnits, class Comm>
+class BMX160GyroImpl : public BMX160ImplBase<Value, BaseUnits, Comm>
+{
+  public:
+    using scalar_t = Value;
+    using base_t = BMX160ImplBase<Value, BaseUnits, Comm>;
+    using registers_t = typename base_t::registers_t;
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    BMX160GyroImpl(const register_access_t& _regs)
+      : base_t(_regs, 1.33e-4) {}
+
+    scalar_t set_fs(scalar_t fs) {
+        return 0;
+    }
+
+    scalar_t get_fs() const {
+        return 0;
+    }
+
+    scalar_t set_odr(scalar_t _freq) {
+        return 0;
+    }
+
+    scalar_t get_odr() const {
+    	return 0;
+    }
+
+  protected:
+    scalar_t gyro_scale_;
+};
+
+template<class Value, class BaseUnits, class Comm>
+class BMX160MagImpl : public BMX160ImplBase<Value, BaseUnits, Comm>
+{
+  public:
+    using scalar_t = Value;
+    using base_t = BMX160ImplBase<Value, BaseUnits, Comm>;
+    using registers_t = typename base_t::registers_t;
+    using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+
+    BMX160MagImpl(const register_access_t& _regs)
+      : base_t(_regs, 1.33e-4) {}
+
+    scalar_t set_fs(scalar_t fs) {
+
+        return 0;
+    }
+
+    scalar_t get_fs() const {
+        return 0;
+    }
+
+    scalar_t set_odr(scalar_t _freq) {
+    	return 0;
+    }
+
+    scalar_t get_odr() const {
+    	return 0;
+    }
+
+  protected:
+    scalar_t mag_scale_;
+};
+} /*namespace internal*/
+
+template<
+  class Value,
+  class Comm = chandra::io::SPIMaster,
+  class AccelUnits = units::mks::m_per_s2,
+  class GyroUnits = units::mks::rad_per_s,
+  class MagUnits = units::mks::T,
+  class TempUnits = units::mks::degC
+>
+class BMX160
+        : public AccelGyroMag<BMX160<Value, Comm, AccelUnits, GyroUnits, MagUnits, TempUnits>, Value, internal::BMX160AccelImpl<Value, AccelUnits, Comm>, internal::BMX160GyroImpl<Value, GyroUnits, Comm>, internal::BMX160MagImpl<Value, MagUnits, Comm>, 3, AccelUnits, GyroUnits, MagUnits>,
+          public Thermometer<BMX160<Value, Comm, AccelUnits, GyroUnits, MagUnits, TempUnits>, Value, TempUnits>
+{
+    protected:
+        using register_access_t = chandra::drivers::RegisterDevice<Comm>;
+        using registers_t = typename internal::BMX160ImplBase<Value, AccelUnits, Comm>::registers_t;
+
+    public:
+        friend class AccelGyroMag<BMX160<Value, Comm, AccelUnits, GyroUnits, MagUnits, TempUnits>, Value, internal::BMX160AccelImpl<Value, AccelUnits, Comm>, internal::BMX160GyroImpl<Value, GyroUnits, Comm>, internal::BMX160MagImpl<Value, MagUnits, Comm>, 3, AccelUnits, GyroUnits, MagUnits>;
+        using base_t = AccelGyroMag<BMX160<Value, Comm, AccelUnits, GyroUnits, MagUnits, TempUnits>, Value, internal::BMX160AccelImpl<Value, AccelUnits, Comm>, internal::BMX160GyroImpl<Value, GyroUnits, Comm>, internal::BMX160MagImpl<Value, MagUnits, Comm>, 3, AccelUnits, GyroUnits, MagUnits>;
+        using scalar_t = typename base_t::scalar_t;
+        using value_t = typename base_t::value_t;
+        using ref_t = BMX160<Value, Comm, AccelUnits, GyroUnits, TempUnits>;
+        using status_t = SensorUpdateStatus<>;
+
+        BMX160(register_access_t _regs) : base_t(_regs), regs_(_regs) {}
+
+        bool init() {
+          enable(true);
+          return valid();
+        }
+
+        bool enable(bool) {
+
+            return true;
+        }
+
+        // RUN SELF TESTS FOR VALIDATATION....
+
+        uint8_t id() { return regs_.byte(registers_t::CHIP_ID); }
+
+        bool valid() const { return id() == 0xD8; }
+
+        status_t update() {
+            status_t status;
+            return status;
+        }
+
+    private:
+        register_access_t regs_;
+        scalar_t accel_scale_;
+        scalar_t gyro_scale_;
+};
+
+} /*namespace drivers*/
+} /*namespace chandra*/
+
+#endif /*CHANDRA_BMX160_H*/
